783b571e8957ea2f26ad35e19e8fd5861eee047f
clementine <cleriv.72@gmail.com>
2024-01-24 15:33:57 +0100

    création de la route permettant de passer un spot en favoris

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -31,8 +31,9 @@ func main() {
 
 	// Set up and start the HTTP server
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/spots", getSpots).Methods("GET")
-	router.HandleFunc("/partialspots", getPartialSpots).Methods("GET")
+	router.HandleFunc("/api/spots", getSpots).Methods("GET")
+    router.HandleFunc("/api/spots", putFavorites).Methods("PUT")
+	// router.HandleFunc("/partialspots", getPartialSpots).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -83,176 +84,33 @@ func getSpots(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(spots)
 }
 
-func getPartialSpots(w http.ResponseWriter, r *http.Request) {
-	myJsonString := `{
-        "records": [
-            {
-                "id": "rec5aF9TjMjBicXCK",
-                "fields": {
-                    "Surf Break": [
-                        "Reef Break"
-                    ],
-                    "Difficulty Level": 4,
-                    "Destination": "Pipeline",
-                    "Geocode": ":grand_cercle_bleu: eyJpIjoiUGlwZWxpbmUsIE9haHUsIEhhd2FpaSIsIm8iOnsic3RhdHVzIjoiT0siLCJmb3JtYXR0ZWRBZGRyZXNzIjoiRWh1a2FpIEJlYWNoIFBhcmssIEhhbGVpd2EsIEhJIDk2NzEyLCBVbml...",
-                    "Influencers": [
-                        "recD1zp1pQYc8O7l2",
-                        "rec1ptbRPxhS8rRun"
-                    ],
-                    "Magic Seaweed Link": "https://magicseaweed.com/Pipeline-Backdoor-Surf-Report/616/",
-                    "Photos": [
-                        {
-                            "id": "attf6yu03NAtCuv5L",
-                            "url": "https://www.lonelyplanet.fr/sites/lonelyplanet/files/styles/article_main_image/public/media/article/image/zachary-shea-mafuz8nh7xq-unsplash_1.jpg?itok=Baex1h1y",
-                            "filename": "thomas-ashlock-64485-unsplash.jpg",
-                            "size": 688397,
-                            "type": "image/jpeg",
-                            "thumbnails": {
-                                "small": {
-                                    "url": "https://dl.airtable.com/yfKxR9ZQqiT7drKxpjdF_small_thomas-ashlock-64485-unsplash.jpg",
-                                    "width": 52,
-                                    "height": 36
-                                },
-                                "large": {
-                                    "url": "https://dl.airtable.com/cFfMuU8NQjaEskeC3B2h_large_thomas-ashlock-64485-unsplash.jpg",
-                                    "width": 744,
-                                    "height": 512
-                                },
-                                "full": {
-                                    "url": "https://dl.airtable.com/psynuQNmSvOTe3BWa0Fw_full_thomas-ashlock-64485-unsplash.jpg",
-                                    "width": 2233,
-                                    "height": 1536
-                                }
-                            }
-                        }
-                    ],
-                    "Peak Surf Season Begins": "2018-07-22",
-                    "Destination State/Country": "Oahu, Hawaii",
-                    "Peak Surf Season Ends": "2018-08-31",
-                    "Address": "Pipeline, Oahu, Hawaii"
-                },
-                "createdTime": "2018-05-31T00:16:16.000Z"
-            },
-            {
-                "id": "recT98Z2El7YYwmc4",
-                "fields": {
-                    "Surf Break": [
-                        "Point Break"
-                    ],
-                    "Difficulty Level": 5,
-                    "Destination": "Skeleton Bay",
-                    "Geocode": ":grand_cercle_bleu: eyJpIjoiU2tlbGV0b24gQmF5LCBOYW1pYmlhIiwibyI6eyJzdGF0dXMiOiJPSyIsImZvcm1hdHRlZEFkZHJlc3MiOiJOYW1pYmlhIiwibGF0IjotMjUuOTE0NDkxOSwibG5nIjoxNC45MDY4NTk...",
-                    "Influencers": [
-                        "recD1zp1pQYc8O7l2",
-                        "rec1ptbRPxhS8rRun"
-                    ],
-                    "Magic Seaweed Link": "https://magicseaweed.com/Skeleton-Bay-Surf-Report/4591/",
-                    "Photos": [
-                        {
-                            "id": "attKinKKZvgdS5A5U",
-                            "url": "https://www.voyageursdumonde.fr/voyage-sur-mesure/magazine-voyage/ShowPhoto/1490/0",
-                            "filename": "brandon-compagne-308937-unsplash.jpg",
-                            "size": 1494974,
-                            "type": "image/jpeg",
-                            "thumbnails": {
-                                "small": {
-                                    "url": "https://dl.airtable.com/Y8970kyrQHWfL6AMkxZQ_small_brandon-compagne-308937-unsplash.jpg",
-                                    "width": 54,
-                                    "height": 36
-                                },
-                                "large": {
-                                    "url": "https://dl.airtable.com/dkQKXoUnTGiofIvg5TJR_large_brandon-compagne-308937-unsplash.jpg",
-                                    "width": 768,
-                                    "height": 512
-                                },
-                                "full": {
-                                    "url": "https://dl.airtable.com/pexuxaQ6D2VV61pyhUwn_full_brandon-compagne-308937-unsplash.jpg",
-                                    "width": 3000,
-                                    "height": 2000
-                                }
-                            }
-                        }
-                    ],
-                    "Peak Surf Season Begins": "2018-09-01",
-                    "Destination State/Country": "Namibia ",
-                    "Peak Surf Season Ends": "2018-11-30",
-                    "Address": "Skeleton Bay, Namibia "
-                },
-                "createdTime": "2018-05-31T00:16:16.000Z"
-            },
-            {
-                "id": "recH2ennHFNOtB1Wt",
-                "fields": {
-                    "Surf Break": [
-                        "Point Break"
-                    ],
-                    "Difficulty Level": 4,
-                    "Destination": "Superbank",
-                    "Geocode": ":grand_cercle_bleu: eyJpIjoiU3VwZXJiYW5rLCBHb2xkIENvYXN0LCBBdXN0cmFsaWEiLCJvIjp7InN0YXR1cyI6Ik9LIiwiZm9ybWF0dGVkQWRkcmVzcyI6IlNuYXBwZXIgUm9ja3MgUmQsIENvb2xhbmdhdHRhIFF...",
-                    "Influencers": [
-                        "recSkJ4HuvzAUBrdd"
-                    ],
-                    "Magic Seaweed Link": "https://magicseaweed.com/Surfers-Paradise-Gold-Coast-Surf-Report/1012/",
-                    "Photos": [
-                        {
-                            "id": "attmtbEOAQteRjz2p",
-                            "url": "https://www.hostelworld.com/blog/wp-content/uploads/2018/02/best-surfing-spots-in-the-world-@wypeoutchef-Hawaii.jpg",
-                            "filename": "jeremy-bishop-80371-unsplash.jpg",
-                            "size": 1524876,
-                            "type": "image/jpeg",
-                            "thumbnails": {
-                                "small": {
-                                    "url": "https://dl.airtable.com/IWP9RPvvSM2pX1sHeigV_small_jeremy-bishop-80371-unsplash.jpg",
-                                    "width": 48,
-                                    "height": 36
-                                },
-                                "large": {
-                                    "url": "https://dl.airtable.com/aBnINo8qQqDvER2f2wGg_large_jeremy-bishop-80371-unsplash.jpg",
-                                    "width": 683,
-                                    "height": 512
-                                },
-                                "full": {
-                                    "url": "https://dl.airtable.com/eZxgLD8hRI2Y27J39LNl_full_jeremy-bishop-80371-unsplash.jpg",
-                                    "width": 3000,
-                                    "height": 2250
-                                }
-                            }
-                        }
-                    ],
-                    "Peak Surf Season Begins": "2018-11-28",
-                    "Destination State/Country": "Gold Coast, Australia",
-                    "Peak Surf Season Ends": "2019-02-01",
-                    "Address": "Superbank, Gold Coast, Australia"
-                },
-                "createdTime": "2018-05-31T00:16:16.000Z"
-            }
-        ],
-        "offset": "recH2ennHFNOtB1Wt"
+
+func putFavorites(w http.ResponseWriter, r *http.Request) {
+    spotID := mux.Vars(r)["id"]
+
+    // Exécutez une requête SELECT pour obtenir la valeur actuelle de "favorites" pour le spot avec l'ID spécifié.
+    row := db.QueryRow("SELECT favorites FROM spot WHERE id = ?", spotID)
+
+    var currentFavorites bool
+    err := row.Scan(&currentFavorites)
+    if err != nil {
+        log.Fatal(err)
+        http.Error(w, "Erreur lors de la récupération des favoris du spot", http.StatusInternalServerError)
+        return
     }
-    `
 
-	var completeSpotList models.CompleteSpotList
-	json.Unmarshal([]byte(myJsonString), &completeSpotList)
+    // Inversez la valeur de currentFavorites (basculer entre true et false)
+    newFavorites := !currentFavorites
 
-	// Crée une nouvelle structure pour stocker les informations réduites
-	var partialSpotList models.PartialSpotList
-	partialSpotList.Records = make([]models.PartialRecord, len(completeSpotList.Records))
+    // Exécutez une requête UPDATE pour mettre à jour la valeur de "favorites" pour le spot avec la nouvelle valeur calculée.
+    _, err = db.Exec("UPDATE spot SET favorites = ? WHERE id = ?", newFavorites, spotID)
+    if err != nil {
+        log.Fatal(err)
+        http.Error(w, "Erreur lors de la mise à jour des favoris du spot", http.StatusInternalServerError)
+        return
+    }
 
-	// Remplit la nouvelle structure avec des informations réduites
-	for i, completeSpot := range completeSpotList.Records {
-		partialSpotList.Records[i].Destination = completeSpot.Fields.Destination
-
-		// Vérifie si la liste des photos n'est pas vide
-		if len(completeSpot.Fields.Photos) > 0 {
-			// Remplit le champ PhotoURL de PartialRecord avec l'URL de la première photo
-			partialSpotList.Records[i].PhotoURL = completeSpot.Fields.Photos[0].URL
-		}
-
-		partialSpotList.Records[i].Address = completeSpot.Fields.Address
-		// Ajoutez d'autres champs que vous souhaitez inclure
-	}
-
-	// Encode la nouvelle structure en JSON et renvoie la réponse
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(partialSpotList)
+    // Répondez avec un statut de succès si tout s'est bien passé.
+    w.WriteHeader(http.StatusOK)
 }
+
